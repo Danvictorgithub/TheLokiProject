58c73331442bb8b2306f54c3f7ad94675cd51efb
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-01-29 01:26:44 +0800

    Added initial design

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1 class="bg-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="h-20 border-b-2">
+    <div class="h-full flex justify-between items-center container mx-auto">
+        <div class="flex gap-4 items-center">
+            <img src="/favicon.png" class="h-12 w-12" alt="">
+            <h1 class="font-bold text-3xl">The Loki Project</h1>
+        </div>
+        <nav class="flex gap-12 text-xl font-medium items-center">
+            <p>All Paths</p>
+            <p>About</p>
+            <p>Donate</p>
+            <p class="px-2 py-1 border-2 rounded-md border-red-600 text-white bg-red-600">sign in</p>
+        </nav>
+    </div>
+</header>
+<main class="h-svh">
+    <article class="container mx-auto h-[600px] flex justify-center items-center flex-col px-4 max-w-[1280px] text-center gap-8">
+        <h2 class="font-bold text-7xl">Master the <span class="text-red-500">basic essentials of web development</span> paving your path to digital success.</h2>
+        <p class="font-medium text-xl">Join 100+ developers learning, building, and deploying their dream web application.</p>
+        <div class="flex gap-4">
+            <button class="px-6 py-2 border-2 border-red-500 bg-red-500 text-white  font-medium rounded-full">Get Started</button>
+            <button class="px-4 py-2 border-2 border-red-500 text-red-500 font-medium">Learn More</button>
+        </div>
+    </article>
+</main>
+<footer class="py-12">
+    <div class="container mx-auto flex h-full justify-between items-start">
+        <div class="">
+            <div class="flex gap-4 items-center">
+                <img src="/favicon.png" class="h-12 w-12" alt="">
+                <h1 class="font-bold text-3xl">The Loki Project</h1>
+            </div>
+            <p class="mt-4">An open-sourced platform for sharing resources about any programming languages you want to learn, Deeply inspired from The Odin Project</p>
+        </div>
+        <div>
+            <p class="font-bold text-2xl">Contribute</p>
+            <p class="mt-4 font-medium text-xl">Github</p>
+        </div>
+        <div>
+            <p class="font-bold text-2xl">Follow Us</p>
+            <p class="mt-4 font-medium text-xl">Github</p>
+            <p class="mt-4 font-medium text-xl">Facebook</p>
+        </div>
+    </div>
+    <div class="container mx-auto flex h-full justify-between items-center">
+        <p class="text-sm text-gray-500">@ 2024 The Loki Project, DanvictorGithub, All rights reserved.</p>
+    </div>
+</footer>
